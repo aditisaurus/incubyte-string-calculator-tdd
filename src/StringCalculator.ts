a9955f08d5f21f7af934a1aab94ee956762e6fd3
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-14 16:59:07 +0530

    feat: support new line characters as number delimiters

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -4,7 +4,10 @@ export class StringCalculator {
       return 0;
     }
 
-    const parts: string[] = numbers.split(",");
+    // Replace new lines with commas to normalize
+    const normalizedNumbers: string = numbers.split("\n").join(",");
+
+    const parts: string[] = normalizedNumbers.split(",");
 
     return parts.reduce((sum: number, num: string) => sum + parseInt(num), 0);
   }
