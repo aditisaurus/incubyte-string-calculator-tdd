f92e8751190cb1048abbbe9dd1b75297e25d836f
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-14 16:48:11 +0530

    feat: implement addition of two comma-separated numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,6 +3,12 @@ export class StringCalculator {
     if (numbers === "") {
       return 0;
     }
+
+    if (numbers.includes(",")) {
+      const parts: string[] = numbers.split(",");
+      return parseInt(parts[0]) + parseInt(parts[1]);
+    }
+
     return parseInt(numbers);
   }
 }
