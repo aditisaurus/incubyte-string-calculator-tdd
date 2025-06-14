037b8d7417baaa27e156168c1f890d4f8dfc13df
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-14 16:47:18 +0530

    test: add failing test for two comma-separated numbers

--- src/__tests__/StringCalculator.test.ts
+++ src/__tests__/StringCalculator.test.ts
@@ -11,4 +11,9 @@ describe("StringCalculator", () => {
     const calculator = new StringCalculator();
     expect(calculator.add("1")).toBe(1);
   });
+
+   test("should return sum of two numbers", () => {
+     const calculator = new StringCalculator();
+     expect(calculator.add("1,5")).toBe(6);
+   });
 });
