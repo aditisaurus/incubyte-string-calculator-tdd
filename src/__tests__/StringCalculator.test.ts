cef449f62a093d1a87be765ea6588e3b0b4fd46b
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-14 16:45:07 +0530

    test: add failing test for single number

--- src/__tests__/StringCalculator.test.ts
+++ src/__tests__/StringCalculator.test.ts
@@ -6,4 +6,9 @@ describe("StringCalculator", () => {
     const calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number itself for a single number", () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("1")).toBe(1);
+  });
 });
